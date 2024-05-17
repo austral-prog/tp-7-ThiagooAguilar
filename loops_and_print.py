3b986cb6ae1e6e8ba4144296aefbb00d271b9e7f
ThiagooAguilar <162721875+ThiagooAguilar@users.noreply.github.com>
2024-05-17 12:31:35 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -10,5 +10,13 @@ def enumerate_list(my_list):
     return new_list
 
 
-def enumerate_backwards(list):
-    return list
+def enumerate_backwards(my_list):
+    new_list=[]
+    current_index= 0
+    number=0 
+    while current_index < len(my_list):
+        if my_list[current_index]!= "": 
+            new_list.append(f"{number}. {my_list[current_index][::-1]}")
+            number+=1
+        current_index+=1
+    return new_list
